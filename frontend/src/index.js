7ef8d985908ea854fde35a1ff891585ad75885f5
Phuc <hoangphucythus@gmail.com>
2023-11-23 17:12:12 +0700

    index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import List from './components/List'
+import List from './components/List';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
